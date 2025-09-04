ecd38bfaaeec15711b0e50e54c35df6a873d6ceb
yvashokkumarreddy <ro200259@rguktong.ac.in>
2025-09-05 00:03:39 +0530

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,10 +1,10 @@
-export default async function handler(req,res) 
-{
-    try{
-        const response = await 
-        fetch("https://blinkgo-full-stack-server.onrender.com");
-        res.status(200).json({ok : true, status: response.status, time:  new Date().toISOString()})
-    } catch (error){
-        res.status(500).json({ok: false, error: error.message});
-    }
-}
+// export default async function handler(req,res) 
+// {
+//     try{
+//         const response = await 
+//         fetch("https://blinkgo-full-stack-server.onrender.com");
+//         res.status(200).json({ok : true, status: response.status, time:  new Date().toISOString()})
+//     } catch (error){
+//         res.status(500).json({ok: false, error: error.message});
+//     }
+// }
