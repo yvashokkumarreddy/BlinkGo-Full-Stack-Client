7d722d0cd150654b0667005e46f7fb38211e1a64
yvashokkumarreddy <ro200259@rguktong.ac.in>
2025-09-14 12:43:57 +0530

    Update adminOrdersSlice.js

--- src/store/adminOrdersSlice.js
+++ src/store/adminOrdersSlice.js
@@ -1,19 +1,19 @@
-// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// const initialValue = {
-//     ordersList : []
-// }
+const initialValue = {
+    ordersList : []
+}
 
-// const adminOrdersSlice = createSlice({
-//     name : 'AdminOrders',
-//     initialState : initialValue,
-//     reducers : {
-//         handleAllOrders : (state,action)=>{
-//             state.ordersList = [...action.payload]
-//         }
-//     }
-// })
+const adminOrdersSlice = createSlice({
+    name : 'AdminOrders',
+    initialState : initialValue,
+    reducers : {
+        handleAllOrders : (state,action)=>{
+            state.ordersList = [...action.payload]
+        }
+    }
+})
 
-// export const {handleAllOrders  } = adminOrdersSlice.Slice.actions
+export const {handleAllOrders  } = adminOrdersSlice.Slice.actions
 
-// export default adminOrdersSlice.reducer
+export default adminOrdersSlice.reducer
