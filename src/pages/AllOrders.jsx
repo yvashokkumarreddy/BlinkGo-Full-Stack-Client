e06d9d79d04227918db64f59bd1ee527ea608c8b
yvashokkumarreddy <ashokreddyyeduru4@gmail.com>
2025-09-25 01:26:22 +0530

    =worked on admin order details page addded shipping label and order confirm popup

--- src/pages/AllOrders.jsx
+++ src/pages/AllOrders.jsx
@@ -64,27 +64,27 @@ export default function AllOrdersPage() {
         <table className="w-full border-collapse text-sm text-left">
           <thead className="bg-blue-400 text-gray-700 text-sm">
             <tr>
-              <th className="p-3 border">Order ID</th>
+              <th className="p-5 border">Order ID</th>
               <th className="p-3 border">Customer</th>
-              <th className="p-3 border">Items</th>
+              <th className="p-1 border">Items</th>
               <th className="p-3 border">Total</th>
               <th className="p-3 border">Payment</th>
               <th className="p-3 border">Status</th>
-              <th className="p-3 border">Date</th>
+              <th className="p-2 border">Date</th>
               <th className="p-3 border">Action</th>
             </tr>
           </thead>
           <tbody>
             {orders.map((order) => (
               <tr key={order.order_Id} className="hover:bg-gray-50">
-                <td className="p-3 border font-medium">{order.order_Id}</td>
+                <td className="p-3 border font-small">{order.order_Id}</td>
                 <td className="p-3 border">
                   {usersMap[order.user_id] || "Unknown User"} <br />
                   <span className="text-xs text-gray-500">
                     {order.shippingAddress?.phone}
                   </span>
                 </td>
-                <td className="p-3 border">
+                <td className="p-2 border-b">
                   {order.orderItems?.map((item) => (
                     <div key={item.productId} className="flex items-center gap-2 mb-1">
                       <img
@@ -103,9 +103,8 @@ export default function AllOrdersPage() {
                 </td>
                 <td className="p-2 border">
                   <div>
-                    {order.paymentMode}{" "}
                     <span
-                      className={`px-2 py-1 ml-1 rounded text-xs font-medium ${
+                      className={`px-1 py-1 ml-1 rounded text-xs font-medium ${
                         order.paymentStatus === "Paid"
                           ? "bg-green-100 text-green-700"
                           : order.paymentStatus === "Pending"
@@ -134,10 +133,11 @@ export default function AllOrdersPage() {
                     {order.status}
                   </span>
                 </td>
-                <td className="p-3 border">
+                <td className="p-2 border">
+                {order.paymentMode}{" "}
                   {new Date(order.createdAt).toLocaleDateString("en-IN", {
                     day: "2-digit",
-                    month: "short",
+                    month: "2-digit",
                     year: "numeric",
                   })}
                 </td>
